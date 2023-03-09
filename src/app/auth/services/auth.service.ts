b470aee7cdd61e1a925195d428d9aede4c45d895
Rolando Oc <admi.jmb@gmail.com>
2023-03-09 06:17:26 -0500

    ⬆️ | added the AuthListenner and isAuth methods

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   Auth,
   UserCredential,
+  NextOrObserver,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
@@ -9,12 +10,18 @@ import {
 
 import { CreateUser } from '../interfaces/create-user.interface';
 import { Credential } from '../interfaces/credential-user.interface';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private _active: boolean = false;
   constructor(public _authFire: Auth) {}
+
+  initAuthListener() {
+    this._authFire.onAuthStateChanged((stateUser) => console.log(stateUser));
+  }
 
   async createUser({ email, password }: CreateUser): Promise<UserCredential> {
     const userCredential = await createUserWithEmailAndPassword(
@@ -32,11 +39,18 @@ export class AuthService {
       email,
       password
     );
-    console.table(userCredential);
     return userCredential;
   }
 
   async signOutUser(): Promise<void> {
     const userCredential = await signOut(this._authFire);
+  }
+
+  isAuth(): Observable<boolean> {
+    this._authFire.onAuthStateChanged(
+      (state) => (this._active = state?.uid != undefined)
+    );
+
+    return of(this._active);
   }
 }
