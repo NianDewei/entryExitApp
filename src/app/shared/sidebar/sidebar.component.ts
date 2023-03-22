aa43a00b722b8eaeb243c20e54f8f7f1ef03de5d
Rolando Oc <admi.jmb@gmail.com>
2023-03-22 18:57:55 -0500

    🏗️ | EntryExit > show user full name

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../auth/services/auth.service';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [],
 })
-export class SidebarComponent implements OnInit {
-  constructor(private _authService: AuthService, private _router: Router) {}
+export class SidebarComponent implements OnInit, OnDestroy {
+  fullName: string = 'Loading...';
+  fullNameSubs!: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+    private _store: Store<AppState>
+  ) {}
+
+  ngOnInit(): void {
+    this._store
+      .select('auth')
+      .subscribe(
+        ({ user }) => (this.fullName = user?.displayName || 'Loading...')
+      );
+  }
+
+  ngOnDestroy(): void {
+    if (this.fullNameSubs) {
+      this.fullNameSubs.unsubscribe();
+    }
+  }
 
   logout() {
     this._authService
